300d4c5f42ba6c3d10a9a07f79afc8c0368bd7b0
Bradley Trager <bradleytrager@Bradleys-MacBook-Pro.local>
2013-10-29 23:07:53 -0400

    Improved styles, added resume data

--- app/initialize.js
+++ app/initialize.js
@@ -44,19 +44,45 @@ App.Resume = Ember.Object.create({
 		}, {
 			title: "C++ Programmer",
 			location: "Cornell University",
-			date: "2005-2007",
-			details:["Created finite-element simulations using C++ and MatLab"]
+			date: "August 2005- May 2007",
+			details: ["Created finite-element simulations using C++ and MatLab"]
 		}]
 	}, {
-		title: 'Experience',
+		title: 'Education',
 		items: [{
-			title: "Senior Web Developer",
-			location: "Team Detroit",
-			date: "September 2013-present"
+			title: "Master of Engineering in Applied Physics",
+			location: "Cornell University, College of Engineering",
+			date: "August 2002 - May 2007"
 		}, {
-			title: "Senior Web Developer",
-			location: "Team Detroit",
-			date: "September 2013-present"
+			title: "Bachelor of Religious Studies",
+			location: "Rabbinical College of America, Mayanot Institute",
+			date: "June 2007 - January 2012"
+		}]
+	}, {
+		title: "Skills",
+		items: [{
+			title: "Javascript",
+			details: ["jQuery, jQuery UI, jQuery Mobile, Knockout.js, Ember.js, Backbone.js, Angular.js"]
+		},{
+
+			title: "Content Management Systems",
+			details: ["Concrete5, Wordpress, Magento"]
+		},{
+
+			title: "Server Side Scripting",
+			details: ["PHP, ASP.NET MVC, C#, Java, Node.js, Ruby"]
+		},{
+
+			title: "Databases",
+			details: ["MySQL, SQL Server, Oracle, MongoDB"]
+		},{
+
+			title: "CSS",
+			details: ["Sass, Less, Compass, Bootstrap, Zurb Foundation"]
+		},{
+
+			title: "Tools",
+			details: ["Git, SVN, Google Webmaster Tools, Google Analytics, Jira, Photoshop"]
 		}]
 	}]
 });
